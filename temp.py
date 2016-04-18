da7455e900d16d0897cfcbc67aaa8876207147b8
MaggieBi <yb89@cornell.edu>
2016-04-18 14:11:50 -0400

    NYC and SF data filtered

--- temp.py
+++ temp.py
@@ -1,11 +1,16 @@
 import csv
 
-LISTING_PATH = "data/nyc_listings.csv"
+LISTING_PATH = "data/sf_listings.csv"
 REVIEW_PATH = "data/nyc_review.csv"
-OUT_LISTING = "data/filtered_nyc_listings.csv"
+OUT_LISTING = "data/filtered_sf_listings.csv"
 OUT_REVIEWS = "data/filtered_nyc_reviews.csv"
 
-FEATURE_LIST = ["id", "listing_url", "name"]
+FEATURE_LIST = ["id", "listing_url", "name", "summary", "space", "description", "thumbnail_url",
+"host_is_superhost", "host_identity_verified", "neighbourhood_cleansed", 
+"neighbourhood_group_cleansed", "zipcode", "room_type", "accommodates", "bedrooms", "beds",
+"amenities", "price", "minimum_nights", "review_scores_rating", "review_scores_accuracy",
+"review_scores_cleanliness", "review_scores_checkin", "review_scores_communication",
+"review_scores_location", "review_scores_value", "instant_bookable", "cancellation_policy"]
 
 in_file = open(LISTING_PATH, "rb")
 reader = csv.DictReader(in_file)
