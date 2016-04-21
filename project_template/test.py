e3719acf6bfbad8c5d191fc16529656777270a1b
liambui <lb598@cornell.edu>
2016-04-21 17:28:13 -0400

    transferred from test ipython notebook

--- project_template/test.py
+++ project_template/test.py
@@ -13,12 +13,46 @@ def read_file(n):
 def _edit(query, msg):
     return Levenshtein.distance(query.lower(), msg.lower())
 
-def find_similar(q):
-	transcripts = read_file(1)
-	result = []
-	for transcript in transcripts:
-		for item in transcript:
-			m = item['text']
-			result.append(((_edit(q, m)), m))
+#def find_similar(q):
+#	transcripts = read_file(1)
+#	result = []
+#	for transcript in transcripts:
+#		for item in transcript:
+#			m = item['text']
+#			result.append(((_edit(q, m)), m))
+#
+#	return sorted(result, key=lambda tup: tup[0])
 
-	return sorted(result, key=lambda tup: tup[0])
+def find_similar(input_description):
+	with open('data/filtered_nyc_listings.csv') as f:
+    NYClistings = [{k: v for k, v in row.items()}
+      for row in csv.DictReader(f, skipinitialspace=True)]
+        
+	with open('data/filtered_sf_listings.csv') as f:
+	  SFlistings = [{k: v for k, v in row.items()}
+			for row in csv.DictReader(f, skipinitialspace=True)]
+	        
+	descript_dict = {}
+	for n in NYClistings:
+	  key = n['id']
+	 	descript_dict[key]=n['description']
+	    
+	for s in SFlistings:
+    key = s['id']
+    descript_dict[key]=s['description']
+
+  n_feats = 5000
+	descriptions = [input_description]+ [descript_dict[d] for d in descript_dict]
+	listing_by_vocab = np.empty((len(descriptions), n_feats))
+
+	tfidf_vec = TfidfVectorizer(min_df = 10, max_df = .8, max_features = n_feats, norm='l2', stop_words = 'english')
+	listing_by_vocab = tfidf_vec.fit_transform(descriptions)
+
+	ranked_list = np.argsort(cosine_similarity(listing_by_vocab[0:1], listing_by_vocab))[0][::-1]
+
+	top_ten = ranked_list[1:11] #first element is the input listing itself
+	top_ten_descriptions = []
+	for i in top_ten:
+		top_ten_descriptions.append(descriptions[i])
+
+	return top_ten_descriptions
--- project_template/views.py
+++ project_template/views.py
@@ -6,14 +6,30 @@ from django.template import loader
 from .form import QueryForm
 from .test import find_similar
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import json
+import numpy as np
+from sklearn.metrics.pairwise import distance_metrics
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import urllib2
+import csv
 
 # Create your views here.
 def index(request):
     output_list = ''
     output=''
     if request.GET.get('search'):
+        headers = {"User-agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}
         search = request.GET.get('search')
-        output_list = find_similar(search)
+        if (search.find('?') == -1):
+            search_id = search[search.find('rooms/')+6:]
+        else:
+            search_id = search[search.find('rooms/')+6:search.find('?')]
+        url = 'https://api.airbnb.com/v2/listings/' + search_id + '?client_id=3092nxybyb0otqw18e8nh5nty&_format=v1_legacy_for_p3'
+        request = urllib2.Request(url, headers=headers)
+        data = json.loads(urllib2.urlopen(request).read())
+        input_description = data['listing']['description']
+        output_list = find_similar(input_description)
         paginator = Paginator(output_list, 10)
         page = request.GET.get('page')
         try:
@@ -22,7 +38,7 @@ def index(request):
             output = paginator.page(1)
         except EmptyPage:
             output = paginator.page(paginator.num_pages)
-    return render_to_response('project_template/index.html', 
-                          {'output': output,
-                           'magic_url': request.get_full_path(),
-                           })
+        return render_to_response('project_template/index.html', 
+                              {'output': output,
+                               'magic_url': request.get_full_path(),
+                               })
