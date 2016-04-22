cf481df2d9fa24ba7d2caf29394fb39ae3923a31
MaggieBi <yb89@cornell.edu>
2016-04-21 20:40:48 -0400

    Index page now displays outputs without formatting

--- project_template/views.py
+++ project_template/views.py
@@ -18,24 +18,27 @@ import csv
 def index(request):
     output_list = ''
     output=''
-    if request.GET.get('search'):
+    print(request.GET)
+    if request.GET.get('AirbnbURL'):
         headers = {"User-agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}
-        search = request.GET.get('search')
+        search = request.GET.get('AirbnbURL')
         if (search.find('?') == -1):
             search_id = search[search.find('rooms/')+6:]
         else:
             search_id = search[search.find('rooms/')+6:search.find('?')]
         url = 'https://api.airbnb.com/v2/listings/' + search_id + '?client_id=3092nxybyb0otqw18e8nh5nty&_format=v1_legacy_for_p3'
-        request = urllib2.Request(url, headers=headers)
-        data = json.loads(urllib2.urlopen(request).read())
+        api_request = urllib2.Request(url, headers=headers)
+        data = json.loads(urllib2.urlopen(api_request).read())
         input_description = data['listing']['description']
         output_list = find_similar(input_description)
-        paginator = Paginator(output_list, 10)
-        page = request.GET.get('page')
-        try:
-            output = paginator.page(page)
-        except PageNotAnInteger:
-            output = paginator.page(1)
-        except EmptyPage:
-            output = paginator.page(paginator.num_pages)
+        output = output_list
+        # paginator = Paginator(output_list, 10)
+        # page = request.GET.get('page')
+        # try:
+        #     output = paginator.page(page)
+        # except PageNotAnInteger:
+        #     output = paginator.page(1)
+        # except EmptyPage:
+        #     output = paginator.page(paginator.num_pages)
+        # print("OUTPUT: " +output)
     return render_to_response('project_template/index.html',{'output': output,'magic_url': request.get_full_path()})
