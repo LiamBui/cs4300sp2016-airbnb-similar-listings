8bd16746ff0a3a0463c841b2bf0256011b15a497
MaggieBi <yb89@cornell.edu>
2016-04-21 21:29:58 -0400

    Outputs displayed fully but need css

--- project_template/test.py
+++ project_template/test.py
@@ -29,20 +29,27 @@ def _edit(query, msg):
 #
 #	return sorted(result, key=lambda tup: tup[0])
 
-def find_similar(input_description):
-	with open('data/filtered_nyc_listings.csv') as f:
-		NYClistings = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+with open('data/filtered_nyc_listings.csv') as f:
+   	NYClistings = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+        
+with open('data/filtered_sf_listings.csv') as f:
+	SFlistings = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
 
-	with open('data/filtered_sf_listings.csv') as f:
-		SFlistings = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
-	        
+def find_similar(input_description):
+	
+	    
+	data = NYClistings + SFlistings
+
 	descript_dict = {}
-	for n in NYClistings:
-		key = n['id']
-		descript_dict[key]=n['description']
-	for s in SFlistings:
-		key = s['id']
-		descript_dict[key]=s['description']
+	for d in data:
+	    key = d['id']
+	    descript_dict[key]=d['description']
+
+	id_to_listing = {}
+	for d in data:
+	    id_to_listing[d['id']] = d
+
+	listing_index_to_id = {index:listing_id for index, listing_id in enumerate([d['id'] for d in data])}
 
 	n_feats = 5000
 	descriptions = [input_description]+ [descript_dict[d] for d in descript_dict]
@@ -53,9 +60,11 @@ def find_similar(input_description):
 
 	ranked_list = np.argsort(cosine_similarity(listing_by_vocab[0:1], listing_by_vocab))[0][::-1]
 
-	top_ten = ranked_list[1:11] #first element is the input listing itself
-	top_ten_descriptions = []
-	for i in top_ten:
-		top_ten_descriptions.append(descriptions[i])
-
-	return top_ten_descriptions
+	top_ten_idx = ranked_list[1:11] #first element is the input listing itself
+	top_ten_listings = []  #top ten listings and their data
+	for i in top_ten_idx:
+	    listing_data = id_to_listing[listing_index_to_id[i]]
+	    sub_dict = {k: listing_data[k] for k in ('listing_url', 'description', 'price', 'bedrooms', 'accommodates', 
+	                                       'summary', 'name')}
+	    top_ten_listings.append(sub_dict)
+	return top_ten_listings
