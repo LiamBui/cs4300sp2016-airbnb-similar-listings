57fbb55cbf242e7634d81d9377161c9926a8d958
liambui <lb598@cornell.edu>
2016-04-29 20:27:44 -0400

    images for amenities

--- mysite/static/script.js
+++ mysite/static/script.js
@@ -66,3 +66,27 @@ $(".morelink").click(function(){
         return false;
     });
 });
+
+function amenities(room_type, accommodates, bedrooms){
+    if(room_type == 'Entire home/apt'){
+        var room_type_icon = '/static/entirehome.png';
+        var room_type_text = 'Entire Home/Apt';
+    }
+    else if(room_type == 'Private room'){
+        var room_type_icon = '/static/private.png';
+        var room_type_text = 'Private Room';
+    }
+    else{
+        var room_type_icon = '/static/shared.png';
+        var room_type_text = 'Shared Room';
+    }
+    var accom_icon = '/static/accommodates.png';
+    var accom_text = 'Accomodates: ' + accommodates;
+    var bedroom_icon = '/static/bedrooms.png';
+    var bedroom_text = 'Bedrooms: ' + bedrooms;
+
+    var html = '<div class = "quickinfo"><img src = "' + room_type_icon + '" class="icons"></img><p class="icon_labels">' + room_type_text + '</p></div><div class = "quickinfo"><img src = "' + accom_icon + '" class="icons"></img><p class="icon_labels">' + accom_text + '</p></div><div class = "quickinfo"><img src = "' + bedroom_icon + '" class="icons"></img><p class="icon_labels">' + bedroom_text + '</p></div>';
+    //$('#listing_info').html(html);
+    //console.log("poop");
+    return html;
+};
