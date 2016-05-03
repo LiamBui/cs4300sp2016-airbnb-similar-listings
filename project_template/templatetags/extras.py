7dd9f888785b231596ff0f8e7e90188ccf77cbd8
MaggieBi <yb89@cornell.edu>
2016-05-02 21:09:50 -0400

    Styling changed

--- project_template/templatetags/extras.py
+++ project_template/templatetags/extras.py
@@ -7,12 +7,12 @@ stop_words = get_stop_words('en')
 
 @register.filter(name='highlight')
 def highlight(value,similar_words):
-	value = '<span class="more">' + value[:210] + '<span class="morecontent"><span>' + value [210:] + '</span><a href="" class="morelink">[Show More]</a></span></span>'
+	value = '<span class="more">' + value[:210] + '<span class="morecontent"><span>' + value [210:] + '</span><a href="" class="morelink">&nbsp;[Show More]</a></span></span>'
 	for word in similar_words:
 		value = value.replace(word, '<span class="highlights">' + word + '</span>')
 	return value
 
 @register.filter(name='more')
 def more(value):
-	value = '<span class="more">' + value[:210] + '<span class="morecontent"><span>' + value [210:] + '</span><a href="" class="morelink">[Show More]</a></span></span>'
+	value = '<span class="more">' + value[:210] + '<span class="morecontent"><span>' + value [210:] + '</span><a href="" class="morelink">&nbsp;[Show More]</a></span></span>'
 	return value
